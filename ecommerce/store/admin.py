1ba3f8c682576c623ce0991596eb8124e226457d
Ignacio Baviera <bavieraignacio52@gmail.com>
2024-09-13 18:44:54 -0300

    final
    checkout, pago, administracion terminado

--- ecommerce/store/admin.py
+++ ecommerce/store/admin.py
@@ -1,10 +1,35 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
+from .models import User, Product, Cart, CartItem, Order, OrderItem
 
 User = get_user_model()
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'email', 'is_staff', 'is_active', 'is_admin')
+    list_display = ('username', 'email', 'is_admin', 'is_active', 'is_staff')
     search_fields = ('username', 'email')
-    ordering = ('-date_joined',)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'stock')
+    search_fields = ('name',)
+    list_filter = ('price',)
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('user',)
+    search_fields = ('user__username',)
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('user', 'total_amount', 'is_paid', 'created_at')
+    search_fields = ('user__username',)
+    list_filter = ('is_paid', 'created_at')
+
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ('cart', 'product', 'quantity')
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ('order', 'product', 'quantity')
